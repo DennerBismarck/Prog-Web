9a8ff3d04ae15fbb277f74d8f89fa064fe8fedb8
Denner Bismarck <67791717+DennerBismarck@users.noreply.github.com>
2022-06-30 16:52:26 -0300

    Password system (Incomplete)

---
+++ Cofre js/cofreJS.js
@@ -0,0 +1,21 @@
+function validacao(){
+    var aux1 = document.getElementById("barrapesquisa");
+    var aux2 = aux1.value;
+}
+function passou(){
+
+}
+function regra1(){
+    
+}
+function regra2(){
+}
+function regra3(){
+    
+}
+function regra4(){
+    
+}
+function regra5(){
+    
+}
